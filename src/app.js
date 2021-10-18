69957bb7e6b35cf9743bf5d6d7a26a84c81c0bd6
denizatuk <dnz.atk96@gmail.com>
2021-10-19 00:29:53 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,13 @@
+console.log("merhaba kodlama io")
+
+// array
+let konutKredileri = ["Konut kredisi","Emlak Konut kredisi", "Kamu Konut Kredisi", "Özel Konut Kredisi"]
+
+
+console.log("<ul>")
+for (let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+
